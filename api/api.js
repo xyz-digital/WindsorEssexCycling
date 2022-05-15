106ae3b1c75c2a70c770b28c55e5571875a72531
Anthony Cardillo <anthony.cardillo@dubsado.com>
2022-05-15 13:11:38 -0400

    added nogo post route to api server

--- api/api.js
+++ api/api.js
@@ -8,15 +8,25 @@ const run = async () => {
   const Nogo = db.collection('nogos')
   
   const app = express();
+  app.use(express.json());
 
   app.get('/api/nogos', async (req, res) => {
     const nogos = await Nogo.find({}).toArray();
     res.json(nogos);
   });
 
+  app.post('/api/nogos', async (req, res) => {
+    const coordinates = req.body.coordinates;
+    await Nogo.insertOne({
+      type: 'LineString',
+      coordinates,
+    });
+    res.sendStatus(200);
+  });
+
   app.listen(4242, () => {
     console.log("http://localhost:4242");
-  })
+  });
 }
 
 run();
