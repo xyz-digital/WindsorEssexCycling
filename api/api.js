def4088c49750c914b1319d2480092fd15e626cd
Anthony Cardillo <anthony.cardillo@dubsado.com>
2022-05-15 13:02:08 -0400

    added nogo get route to api server

--- api/api.js
+++ api/api.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const mongodb =  require("mongodb");
 
-const app = express();
+const run = async () => {
+  const client = new mongodb.MongoClient('mongodb://localhost:27017');
+  const connection = await client.connect();
+  const db = connection.db('windsoressexcycling');
+  const Nogo = db.collection('nogos')
+  
+  const app = express();
 
-app.listen(4242, () => {
-  console.log("http://localhost:4242");
-})
+  app.get('/api/nogos', async (req, res) => {
+    const nogos = await Nogo.find({}).toArray();
+    res.json(nogos);
+  });
+
+  app.listen(4242, () => {
+    console.log("http://localhost:4242");
+  })
+}
+
+run();
