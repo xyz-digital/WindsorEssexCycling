02d08ee9dda468caaccad6a8cc4744845cee2b0e
lpalazzi <lpalazzi@outlook.com>
2022-06-17 11:54:42 -0700

    add BC/Calgary to segment update script

--- backend/update_segments.sh
+++ backend/update_segments.sh
@@ -2,10 +2,37 @@
 
 update_segments () {
   mkdir -p ./misc/segments4;
-  curl -o ./misc/segments4/W80_N40.rd5 http://brouter.de/brouter/segments4/W80_N40.rd5
-  curl -o ./misc/segments4/W80_N45.rd5 http://brouter.de/brouter/segments4/W80_N45.rd5
-  curl -o ./misc/segments4/W85_N40.rd5 http://brouter.de/brouter/segments4/W85_N40.rd5
-  curl -o ./misc/segments4/W85_N45.rd5 http://brouter.de/brouter/segments4/W85_N45.rd5
+
+  # SW Ontario
+  SW_corner_lat=40
+  SW_corner_lon=-90
+  NE_corner_lat=50
+  NE_corner_lon=-70
+  for W in $(seq $((-NE_corner_lon - 5)) 5 $((0 - SW_corner_lon)))
+  do
+    for N in $(seq $SW_corner_lat 5 $((NE_corner_lat - 5)))
+    do
+      echo ""
+      echo "Downloading W${W}_N${N}"
+      curl -o ./misc/segments4/W${W}_N${N}.rd5 http://brouter.de/brouter/segments4/W${W}_N${N}.rd5
+    done
+  done
+
+  # BC/Calgary
+  SW_corner_lat=45
+  SW_corner_lon=-135
+  NE_corner_lat=55
+  NE_corner_lon=-110
+  for W in $(seq $((-NE_corner_lon - 5)) 5 $((0 - SW_corner_lon)))
+  do
+    for N in $(seq $SW_corner_lat 5 $((NE_corner_lat - 5)))
+    do
+      echo ""
+      echo "Downloading W${W}_N${N}"
+      curl -o ./misc/segments4/W${W}_N${N}.rd5 http://brouter.de/brouter/segments4/W${W}_N${N}.rd5
+    done
+  done
+
 }
 
 update_segments
